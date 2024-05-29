c969dc10a2dce693575614af290c9db3c893380a
jerin <jerinkunnamkada@gmail.com>
2024-05-29 11:43:06 +0530

    tab

--- src/app/components/productDetailsPage/ProductTab.jsx
+++ src/app/components/productDetailsPage/ProductTab.jsx
@@ -1,9 +1,10 @@
-/* "use client";
+"use client";
+import { FaRegUserCircle } from "react-icons/fa";
+
 import React, { useState } from "react";
 
 const ProductTab = ({ product }) => {
   const [activeTab, setActiveTab] = useState(0);
-  console.log(product);
 
   const tabs = [
     {
@@ -11,29 +12,51 @@ const ProductTab = ({ product }) => {
       title: "Description",
       content: product.long_desc,
     },
-    { id: 1, title: "Reviews", content:product.reviews },
+    { 
+      id: 1, 
+      title: "Reviews", 
+      content: product.reviews 
+    },
   ];
 
   return (
-    <div claĀ
-      <div cĀy-200">
-        {tabĀ
-          <bĀ
-            Ā
-            Ā-medium focus:outline-none ${
-            Ā
-            Ātext-blue-600"
-            Ā
-            Ā
-            Ā)}
+    <div className="w-full my-10 ">
+      <div className="flex border-b border-gray-200">
+        {tabs.map((tab) => (
+          <button
+            key={tab.id}
+            className={`py-2 px-4 text-sm font-medium focus:outline-none transition-colors duration-300 ${
+              activeTab === tab.id
+                ? "border-b-2 border-blue-500 text-blue-600"
+                : "text-gray-500 hover:text-blue-600"
+            }`}
+            onClick={() => setActiveTab(tab.id)}
           >
-            ĀĀĀĀĀĀĀ
-          </Ā
+            {tab.title}
+          </button>
         ))}
-      </div>Ā
+      </div>
       <div className="mt-4">
         {tabs.map(
-          (tab) => activeTab === tab.id && <div className="text-base text-[#030712] font-light" key={tab.id}>{tab.content}</div>
+          (tab) =>
+            activeTab === tab.id && (
+              <div key={tab.id}>
+                {Array.isArray(tab.content) ? (
+                  tab.content.map((review, index) => (
+                    <div key={index} className="mb-4 flex items-center gap-3">
+                      <FaRegUserCircle size={35} />
+
+                      <div>
+                        <p className="font-semibold">{review.name}</p>
+                        <p className="text-gray-500 text-sm">{review.review}</p>
+                      </div>
+                    </div>
+                  ))
+                ) : (
+                  <p  >{tab.content}</p>
+                )}
+              </div>
+            )
         )}
       </div>
     </div>
@@ -41,4 +64,3 @@ const ProductTab = ({ product }) => {
 };
 
 export default ProductTab;
- */
